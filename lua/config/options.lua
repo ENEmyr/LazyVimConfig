d8f8b4dd40f20891506820f4cb6b9da5affb0883
ENEmy <enemyr@pm.me>
2024-05-19 08:42:26 +0700

    feat(options.lua): refactor Neovim settings
    The Neovim settings in options.lua have been refactored for better
    readability and maintainability. The settings are now set using the
    vim.g and vim.o tables instead of vim.cmd. This change also introduces
    new settings for Neovide such as window blur, hiding mouse when typing,
    and remembering window size. The transparency setting has been updated
    to use a function that converts the transparency level to a hexadecimal
    value for the background color.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,18 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.cmd("let g:neovide_scale_factor = 0.8")
-vim.cmd("let g:neovide_refresh_rate_idle = 5")
-vim.cmd("let g:neovide_refresh_rate = 144")
-vim.cmd("let g:neovide_cursor_vfx_mode = 'wireframe'")
--- vim.cmd("let g:neovide_floating_blur_amount_x = 2.0")
--- vim.cmd("let g:neovide_floating_blur_amount_y = 2.0")
--- vim.cmd("let g:neovide_transparency = 0.9")
--- vim.cmd("let g:transparency = 0.5")
--- vim.cmd("let g:neovide_background_color = '#0f1117'.printf('%x', float2nr(255 * g:transparency))")
-vim.cmd("let g:neovide_profiler = v:false")
-vim.cmd("let g:neovide_input_macos_alt_is_meta = v:false")
-vim.cmd("let g:neovide_input_use_logo = v:true")
-vim.cmd("set guifont=CaskaydiaCove\\ Nerd\\ Font:h14")
-vim.cmd("let g:pydocstring_doq_path = '/home/enemy/miniconda3/bin/doq'")
-vim.cmd("let g:pydocstring_enable_mapping = 0")
+
+local alpha = function()
+  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+
+vim.g.neovide_profiler = false
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_refresh_rate = 144
+vim.g.neovide_cursor_vfx_mode = "wireframe"
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_window_blurred = true
+vim.g.neovide_input_macos_alt_is_meta = false
+vim.g.neovide_input_use_logo = true
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_transparency = 0.95
+vim.g.transparency = 0.95
+vim.g.neovide_background_color = "#0f1117" .. alpha()
+vim.o.guifont = "CaskaydiaCove Nerd Font:h14"
+
+vim.g.pydocstring_doq_path = "/home/enemy/miniconda3/bin/doq"
+vim.g.pydocstring_enable_mapping = 0
