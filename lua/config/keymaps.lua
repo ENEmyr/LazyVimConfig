61e79690a769460fab063b979348536d77ba0ba4
ENEmyr <enemyr@pm.me>
2025-02-14 07:26:56 +0700

    feat: add new key map for thai typing in this vim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -86,3 +86,125 @@ wk.add({
   { "<leader>ww", "<cmd>HopWord<CR>", desc = "Word" },
   { "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv", desc = "Run visual selection", mode = "v" },
 })
+
+-- mapping for thai keys
+
+-- Assumes kedmanee key bindings, for now.
+-- Pull requests welcome for more mappings ; - )
+
+-- keys that are already mapped are the keys that Vim use by default (because
+-- they are available in English keyboard). So we shouldn't remap it to something
+-- else without user's acknowledgement
+-- ___________________________
+--                KEY MAPPINGS
+-- :map \- ` " already mapped
+vim.api.nvim_set_keymap("n", "ๅ", "1", { noremap = true })
+-- :map / 2 " already mapped
+-- :map _ 3 " already mapped
+vim.api.nvim_set_keymap("n", "ภ", "4", { noremap = true })
+vim.api.nvim_set_keymap("n", "ถ", "5", { noremap = true })
+vim.api.nvim_set_keymap("n", "ุ", "6", { noremap = true })
+vim.api.nvim_set_keymap("n", "ึ", "7", { noremap = true })
+vim.api.nvim_set_keymap("n", "ค", "8", { noremap = true })
+vim.api.nvim_set_keymap("n", "ต", "9", { noremap = true })
+vim.api.nvim_set_keymap("n", "จ", "0", { noremap = true })
+vim.api.nvim_set_keymap("n", "ข", "-", { noremap = true })
+vim.api.nvim_set_keymap("n", "ช", "=", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "%", "~", { noremap = true })
+-- :map \+ ! " already mapped
+vim.api.nvim_set_keymap("n", "๑", "@", { noremap = true })
+vim.api.nvim_set_keymap("n", "๒", "#", { noremap = true })
+vim.api.nvim_set_keymap("n", "๓", "$", { noremap = true })
+vim.api.nvim_set_keymap("n", "๔", "%", { noremap = true })
+vim.api.nvim_set_keymap("n", "ู", "^", { noremap = true })
+vim.api.nvim_set_keymap("n", "฿", "&", { noremap = true })
+vim.api.nvim_set_keymap("n", "๕", "*", { noremap = true })
+vim.api.nvim_set_keymap("n", "๖", "(", { noremap = true })
+vim.api.nvim_set_keymap("n", "๗", ")", { noremap = true })
+vim.api.nvim_set_keymap("n", "๘", "_", { noremap = true })
+vim.api.nvim_set_keymap("n", "๙", "+", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "ๆ", "q", { noremap = true })
+vim.api.nvim_set_keymap("n", "ไ", "w", { noremap = true })
+vim.api.nvim_set_keymap("n", "ำ", "e", { noremap = true })
+vim.api.nvim_set_keymap("n", "พ", "r", { noremap = true })
+vim.api.nvim_set_keymap("n", "ะ", "t", { noremap = true })
+vim.api.nvim_set_keymap("n", "ั", "y", { noremap = true })
+vim.api.nvim_set_keymap("n", "ี", "u", { noremap = true })
+vim.api.nvim_set_keymap("n", "ร", "i", { noremap = true })
+vim.api.nvim_set_keymap("n", "น", "o", { noremap = true })
+vim.api.nvim_set_keymap("n", "ย", "p", { noremap = true })
+vim.api.nvim_set_keymap("n", "บ", "[", { noremap = true })
+vim.api.nvim_set_keymap("n", "ล", "]", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฟ", "a", { noremap = true })
+vim.api.nvim_set_keymap("n", "ห", "s", { noremap = true })
+vim.api.nvim_set_keymap("n", "ก", "d", { noremap = true })
+vim.api.nvim_set_keymap("n", "ด", "f", { noremap = true })
+vim.api.nvim_set_keymap("n", "เ", "g", { noremap = true })
+vim.api.nvim_set_keymap("n", "เเ", "gg", { noremap = true })
+vim.api.nvim_set_keymap("n", "้", "h", { noremap = true })
+vim.api.nvim_set_keymap("n", "่", "j", { noremap = true })
+vim.api.nvim_set_keymap("n", "า", "k", { noremap = true })
+vim.api.nvim_set_keymap("n", "ส", "l", { noremap = true })
+vim.api.nvim_set_keymap("n", "ว", ";", { noremap = true })
+vim.api.nvim_set_keymap("n", "ง", "'", { noremap = true })
+vim.api.nvim_set_keymap("n", "ผ", "z", { noremap = true })
+vim.api.nvim_set_keymap("n", "ป", "x", { noremap = true })
+vim.api.nvim_set_keymap("n", "แ", "c", { noremap = true })
+vim.api.nvim_set_keymap("n", "อ", "v", { noremap = true })
+vim.api.nvim_set_keymap("n", "ิ", "b", { noremap = true })
+vim.api.nvim_set_keymap("n", "ื", "n", { noremap = true })
+vim.api.nvim_set_keymap("n", "ท", "m", { noremap = true })
+vim.api.nvim_set_keymap("n", "ม", ",", { noremap = true })
+vim.api.nvim_set_keymap("n", "ใ", ".", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฝ", "/", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "๐", "Q", { noremap = true })
+-- :map \" W "key already mapped
+vim.api.nvim_set_keymap("n", "ฎ", "E", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฑ", "R", { noremap = true })
+vim.api.nvim_set_keymap("n", "ธ", "T", { noremap = true })
+vim.api.nvim_set_keymap("n", "ํ", "Y", { noremap = true })
+vim.api.nvim_set_keymap("n", "๊", "U", { noremap = true })
+vim.api.nvim_set_keymap("n", "ณ", "I", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฯ", "O", { noremap = true })
+vim.api.nvim_set_keymap("n", "ญ", "P", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฐ", "{", { noremap = true })
+-- :map , \} " already mapped
+-- :map ฅ | " no mapping found
+vim.api.nvim_set_keymap("n", "ฤ", "A", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฆ", "S", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฏ", "D", { noremap = true })
+vim.api.nvim_set_keymap("n", "โ", "F", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฌ", "G", { noremap = true })
+vim.api.nvim_set_keymap("n", "็", "H", { noremap = true })
+vim.api.nvim_set_keymap("n", "๋", "J", { noremap = true })
+vim.api.nvim_set_keymap("n", "ษ", "K", { noremap = true })
+vim.api.nvim_set_keymap("n", "ศ", "L", { noremap = true })
+vim.api.nvim_set_keymap("n", "ซ", ":", { noremap = true })
+-- :map \. \" " key already mapped
+-- :map \( Z " key already mapped
+-- :map \) X " key already mapped
+vim.api.nvim_set_keymap("n", "ฉ", "C", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฮ", "V", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฺ", "B", { noremap = true })
+vim.api.nvim_set_keymap("n", "์", "N", { noremap = true })
+-- :map ? M " key already mapped
+vim.api.nvim_set_keymap("n", "ฒ", "<", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฬ", ">", { noremap = true })
+vim.api.nvim_set_keymap("n", "ฦ", "?", { noremap = true })
+
+-- Scrolling relative to cursor
+-- For help, type :help scroll-cursor
+vim.api.nvim_set_keymap("n", "ผผ", "zz", { noremap = true })
+vim.api.nvim_set_keymap("n", "ผะ", "zt", { noremap = true })
+vim.api.nvim_set_keymap("n", "ผิ", "zb", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<c-ี>", "<c-u>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<c-ก>", "<c-d>", { noremap = true })
+-- TODO: fix these line to make it work
+-- vim.api.nvim_set_keymap("n", "<s-้>", "<s-h>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<s-ส>", "<s-l>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<c-้>", "<c-h>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<c-ส>", "<c-l>", { noremap = true })
